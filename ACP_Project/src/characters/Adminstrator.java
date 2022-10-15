ac555f9c84fbd80a84f8224ae3b4df4df1d5db08
Mohammed_Jasim <92684315+Mohammed-Jasm@users.noreply.github.com>
2022-10-15 21:05:15 +0300

    Add files via upload

---
+++ ACP_Project/src/characters/Adminstrator.java
@@ -0,0 +1,15 @@
+package characters;
+
+public class Adminstrator {
+    private final String id = "00000000";
+    private final String password = "admin_123_admin";
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
