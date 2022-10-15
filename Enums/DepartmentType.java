b3d03fdba184f1086537ad29267dba97cefb030b
Emad Adil Qadr <115411050+Emad-Adil-Qadr@users.noreply.github.com>
2022-10-15 20:58:06 +0300

    Delete Enums directory

--- Enums/DepartmentType.java
+++
@@ -1,7 +0,0 @@
-package Enums;
-
-public enum DepartmentType {
-    Zansty,
-    Wezhaiy,
-    Ayny,
-}
--- Enums/StudentType.java
+++
@@ -1,7 +0,0 @@
-package Enums;
-
-public enum StudentType {
-    Zansty,
-    Wezhaiy,
-    Ayny,
-}
