ff0d5beac6757b66e3efb436f03879d2b55e6c85
Twana Abubakr <95369929+twana18@users.noreply.github.com>
2022-10-21 19:36:26 +0300

    Using record instead of class

--- ACP_Project/src/university_information/Department.java
+++ ACP_Project/src/university_information/Department.java
@@ -1,34 +1,5 @@
 package university_information;
 
-import Enums.DepartmentType;
+import Enums.SchoolStudyType;
 
-public class Department {
-    private final String id;
-    private final String name;
-    private int capacity;
-    private final DepartmentType departmentType;
-
-    public Department(String departmentId, String departmentName, DepartmentType departmentType, int departmentCapacity) {
-        id = departmentId;
-        name = departmentName;
-        capacity = departmentCapacity;
-        this.departmentType = departmentType;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-}
+public record Department (String departmentId, String departmentName, SchoolStudyType type, int departmentCapacity) { }
