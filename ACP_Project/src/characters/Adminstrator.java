67a66b9bf97db901118f0b7b97a4ac30250382ad
Twana Abubakr <95369929+twana18@users.noreply.github.com>
2022-10-21 19:05:16 +0300

    Using record instead of class

--- ACP_Project/src/characters/Adminstrator.java
+++ ACP_Project/src/characters/Adminstrator.java
@@ -1,15 +1,3 @@
 package characters;
 
-public class Adminstrator {
-    private final String id = "00000000";
-    private final String password = "admin_123_admin";
-
-    public String getId() {
-        return id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-}
+public record Adminstrator(String id,String name,String password){}
