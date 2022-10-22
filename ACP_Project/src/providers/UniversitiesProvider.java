ce0cee230d32f66604d766e71596f8ac1c44cf2a
Emad Adil Qadr <115411050+Emad-Adil-Qadr@users.noreply.github.com>
2022-10-22 22:37:38 +0300

    Some improvements
    1. UNIVERSITIES: a HashMap that contains universities.
    2. gettingData: fetching data.
    3.  addUnversity: a method that adminstrator use it to add universities.
    4. removeUniversity: a method that adminstrator use it to remove universities.

--- ACP_Project/src/providers/UniversitiesProvider.java
+++ ACP_Project/src/providers/UniversitiesProvider.java
@@ -1,5 +1,24 @@
 package providers;
 
+import university_information.University;
+import java.util.HashMap;
+
 public class UniversitiesProvider {
-    
+    public static HashMap<String, University> UNIVERSITIES = new HashMap<>();
+
+    public static void gettingData() {
+    	UNIVERSITIES.put("UN01",new University("UN01","Salahaddin","Hawler"));
+    	UNIVERSITIES.put("UN02",new University("UN02","Hawler Medicine","Hawler"));
+    	UNIVERSITIES.put("UN03",new University("UN03","Slemani","Slemani"));
+    	UNIVERSITIES.put("UN04",new University("UN04","Duhok","Duhok"));
+    	UNIVERSITIES.put("UN05",new University("UN05","PolyTechnique","Hawler"));
+    	UNIVERSITIES.put("UN06",new University("UN06","PolyTechnique","Slemani"));
+    }
+    public static void addUnversity(String location, String universityID, String universityName){
+        UNIVERSITIES.put(universityID, new University(universityID, universityName, location));
+    }
+    public static void removeUniversity(String universityID){
+            UNIVERSITIES.remove(universityID);
+    }
 }
+
