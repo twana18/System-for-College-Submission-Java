ec09d0b92dd414e4b83511b4b0461d58374dd16f
Hast Sarbaz Sadraldin <ensw04620@student.su.edu.krd>
2022-10-22 22:39:18 +0300

    Adding & Removing Colleges
    Creating a hashmap type of PairContainer (Key) and College (Value) Classes that we use to add colleges to the hashmap, also we can remove colleges in it.

--- ACP_Project/src/providers/CollegesProvider.java
+++ ACP_Project/src/providers/CollegesProvider.java
@@ -1,5 +1,21 @@
 package providers;
 
+import Custom_Generic_Classes_Interfaces.PairContainer;
+import university_information.College;
+
+import java.util.HashMap;
+
 public class CollegesProvider {
+    public static HashMap<PairContainer<String, String>, College> COLLEGES = new HashMap<>();
     
+    public static void gettingData() { }
+    
+    public static void addCollege(String universityID, String collegeID, String collegeName){
+            COLLEGES.put(new PairContainer<>(universityID,collegeID), new College(collegeID,collegeName));
+    }
+
+    public static void removeCollege(String universityID, String collegeID){
+            COLLEGES.remove(new PairContainer<>(universityID,collegeID));
+    }
 }
+
