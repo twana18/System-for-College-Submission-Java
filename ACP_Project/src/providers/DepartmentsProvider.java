a68ba242059e96407ca3fec2cb5d47fec87b2eb4
Mohammed_Jasim <92684315+Mohammed-Jasm@users.noreply.github.com>
2022-10-22 22:42:51 +0300

    Some Department Provider improvements,
    1-Getting data: This one will be used for importing our data for the admin 
    2-addDepartment: This one will add department for a specific college in a specific university, all these will be done by one of admins.
    3-removeDepartment; This one will remove a specific department for a specific college in a specific university, all these will be done by one of admins.

--- ACP_Project/src/providers/DepartmentsProvider.java
+++ ACP_Project/src/providers/DepartmentsProvider.java
@@ -1,5 +1,34 @@
 package providers;
 
+import Custom_Generic_Classes_Interfaces.TripleContainer;
+import Enums.SchoolStudyType;
+import university_information.Department;
+
+import java.util.HashMap;
+
 public class DepartmentsProvider {
+    public static HashMap<TripleContainer<String, String, String>, Department> DEPARTMENTSforZansty = new HashMap<>();
+    public static HashMap<TripleContainer<String, String, String>, Department> DEPARTMENTSforWezhaiy = new HashMap<>();
+    public static HashMap<TripleContainer<String, String, String>, Department> DEPARTMENTSforAyny = new HashMap<>();
+
+    public static void gettingData(){ }
+
+    public static void addDepartment(String universityID,String collegeID, String deptID, String deptName, SchoolStudyType type, int deptCapacity){
+        if (type == SchoolStudyType.Zansty){
+            DEPARTMENTSforZansty.put(new TripleContainer<>(universityID, collegeID, deptID), new Department(deptID, deptName,type,deptCapacity));
+        }
+        if (type == SchoolStudyType.Wezhaiy){
+            DEPARTMENTSforWezhaiy.put(new TripleContainer<>(universityID, collegeID, deptID), new Department(deptID, deptName,type,deptCapacity));
+        }
+        if (type == SchoolStudyType.Ayny){
+            DEPARTMENTSforAyny.put(new TripleContainer<>(universityID, collegeID, deptID), new Department(deptID, deptName,type,deptCapacity));
+        }
+    }
+
+    public static void removeDepartment(String universityID,String collegeID, String deptID){
+                DEPARTMENTSforZansty.remove(new TripleContainer<>(universityID,collegeID, deptID));
+                DEPARTMENTSforWezhaiy.remove(new TripleContainer<>(universityID,collegeID, deptID));
+                DEPARTMENTSforAyny.remove(new TripleContainer<>(universityID,collegeID, deptID));
+    }
     
 }
