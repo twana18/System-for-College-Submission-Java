ce9e7552b89a9cdb06c01ee4e0a0d3b7df2748ff
Shallaw Fars <95315844+Shallawfars@users.noreply.github.com>
2022-10-22 21:50:37 +0300

    Add files via upload

---
+++ ACP_Project/src/providers/AdminstratorsProvider.java
@@ -0,0 +1,30 @@
+package providers;
+
+import characters.Adminstrator;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AdminstratorsProvider {
+    public static final List<Adminstrator> adminstrators = Arrays.asList(
+            new Adminstrator("A01", "Zhanyar Fatah Razaq", "12345678"),
+            new Adminstrator("A02", "Raz Ahmed Shwan"  , "12345678"),
+            new Adminstrator("A03", "Ahmed Ali Hama", "12345678"),
+            new Adminstrator("A04", "Rozhgar Namiq Rasull", "12345678"),
+            new Adminstrator("A05", "Siako Nadr Jalal", "12345678"),
+            new Adminstrator("A06", "Aso Shema Ali", "12345678"),
+            new Adminstrator("A07", "Rezhwan Nury Kawa", "12345678"),
+            new Adminstrator("A08", "Zana Aziz Khidir", "12345678"),
+            new Adminstrator("A09", "Mustafa Ako Nuri", "12345678"),
+            new Adminstrator("A10", "Salam Zyad Hawta", "12345678"),
+            new Adminstrator("A11", "Bawar Bayiz Muhammed", "12345678"),
+            new Adminstrator("A12", "Dara Aso Dara", "12345678"),
+            new Adminstrator("A13", "Dlzar Awlla Hamad", "12345678"),
+            new Adminstrator("A14", "Shno Aziz Mirza", "12345678"),
+            new Adminstrator("A15", "Shaho Fars Ali", "12345678"),
+            new Adminstrator("A16", "Ali Hamad Husen", "12345678"),
+            new Adminstrator("A17", "Ania Ari Ali", "12345678"),
+            new Adminstrator("A18", "Rahand Jalal Hamid", "12345678"),
+            new Adminstrator("A19", "Dlyar Zhewar Hawez", "12345678"),
+            new Adminstrator("A20", "Shallaw Ahmad Muhamad", "12345678"));
+}
