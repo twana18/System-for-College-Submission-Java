96928d42f27e3ff004d3cde2ed0d9c266c16785a
Twana Abubakr <95369929+twana18@users.noreply.github.com>
2022-11-12 15:13:09 +0300

    fetch method was added.

--- ACP_Project/src/providers/CollegesProvider.java
+++ ACP_Project/src/providers/CollegesProvider.java
@@ -7,11 +7,11 @@ import java.util.HashMap;
 import java.util.List;
 
 public class CollegesProvider {
-    public static HashMap<List<String>, College> COLLEGES = new HashMap<>();
+    private static HashMap<List<String>, College> COLLEGES = new HashMap<>();
     private static boolean isChanged = false;
-    
-    public static void getColleges() throws IOException, ClassNotFoundException {
-        ObjectInputStream reader = new ObjectInputStream(new FileInputStream("src/DataFiles/Colleges.txt"));
+
+    public static void fetchColleges() throws IOException, ClassNotFoundException {
+        ObjectInputStream reader = new ObjectInputStream(new FileInputStream("ACP_Project/src/DataFiles/Colleges.txt"));
         COLLEGES = (HashMap<List<String>, College>) reader.readObject();
         reader.close();
         if (isChanged) {
@@ -19,7 +19,11 @@ public class CollegesProvider {
             System.out.println("Changes canceled");
         }
     }
-    
+
+    public static HashMap<List<String>, College> getCOLLEGES() {
+        return COLLEGES;
+    }
+
     public static void addCollege(String universityID, String collegeID, String collegeName) {
         COLLEGES.put(Arrays.asList(universityID, collegeID), new College(collegeID, collegeName));
         if (!isChanged) {
@@ -33,36 +37,17 @@ public class CollegesProvider {
             isChanged = true;
         }
     }
+    
     public static void submitChanges() throws IOException, ClassNotFoundException {
         if (isChanged) {
-            ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("src/DataFiles/Colleges.txt"));
+            ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("ACP_Project/src/DataFiles/Colleges.txt"));
             outputStream.writeObject(COLLEGES);
             outputStream.close();
             COLLEGES.clear();
             isChanged = false;
-            getColleges();
+            fetchColleges();
         } else {
             System.out.println("Please add or remove at least a College");
         }
     }
-    public static void testMethod() throws IOException, ClassNotFoundException {
-        System.out.println("Colleges before getting data: " + COLLEGES);
-        getColleges();
-        System.out.println("Colleges after getting data: " + COLLEGES);
-        submitChanges();
-        addCollege("1", "this is a test", "test");
-        addCollege("2", "this is a test", "test");
-        getColleges();
-        submitChanges();
-        addCollege("1", "this is a test", "test");
-        addCollege("2", "this is a test", "test");
-        submitChanges();
-        System.out.println("Colleges after submitting changes: " + COLLEGES);
-        removeCollege("UN01","COL01");
-        removeCollege("UN05","COL04");
-        submitChanges();
-        System.out.println("Colleges after submitting changes: " + COLLEGES);
-    }
-
 }
-
