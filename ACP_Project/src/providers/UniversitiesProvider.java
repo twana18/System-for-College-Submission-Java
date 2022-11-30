97ca4bb43dd7a31e9be484b5e84252ed02f7cfca
Emad Adil Qadr <115411050+Emad-Adil-Qadr@users.noreply.github.com>
2022-11-30 20:16:10 +0300

    Update UniversitiesProvider.java

--- ACP_Project/src/providers/UniversitiesProvider.java
+++ ACP_Project/src/providers/UniversitiesProvider.java
@@ -1,22 +1,27 @@
 package providers;
 
-import university_information.University;
+import Models.University;
 
 import java.io.*;
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class UniversitiesProvider {
-    private static HashMap<String, University> UNIVERSITIES = new HashMap<>();
+    private static ConcurrentHashMap<String, University> UNIVERSITIES = new ConcurrentHashMap<>();
     private static boolean isChanged = false;
 
-    public static void fetchUniversities() throws IOException, ClassNotFoundException {
-        ObjectInputStream reader = new ObjectInputStream(new FileInputStream("ACP_Project/src/DataFiles/Universities.txt"));
-        UNIVERSITIES = (HashMap<String, University>) reader.readObject();
-        reader.close();
-        if (isChanged) {
-            isChanged = false;
-            System.out.println("Changes canceled");
+    public static void fetchUniversities() {
+        try {
+            ObjectInputStream reader = new ObjectInputStream(new FileInputStream("ACP_Project/src/DataFiles/Universities.txt"));
+            UNIVERSITIES = (ConcurrentHashMap<String, University>) reader.readObject();
+            reader.close();
+            if (isChanged) {
+                isChanged = false;
+                System.out.println("Changes canceled");
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Cannot fetch universities: " + e.getMessage());
         }
+
     }
 
     public static void addUniversity(String universityID, String universityName, String location) {
@@ -33,21 +38,26 @@ public class UniversitiesProvider {
         }
     }
 
-    public static void submitChanges() throws IOException, ClassNotFoundException {
-        if (isChanged) {
-            ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("ACP_Project/src/DataFiles/Universities.txt"));
-            outputStream.writeObject(UNIVERSITIES);
-            outputStream.close();
-            UNIVERSITIES.clear();
-            isChanged = false;
-            fetchUniversities();
-        } else {
-            System.out.println("Please add or remove at least a University");
+    public static void submitChanges() {
+        try {
+            if (isChanged) {
+                ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("ACP_Project/src/DataFiles/Universities.txt"));
+                outputStream.writeObject(UNIVERSITIES);
+                outputStream.close();
+                UNIVERSITIES.clear();
+                isChanged = false;
+                fetchUniversities();
+            } else {
+                System.out.println("Please add or remove at least a University");
+            }
+        } catch (IOException e) {
+            System.out.println("Cannot submit changes in universities");
         }
+
     }
 
-    public static HashMap<String, University> getUNIVERSITIES() {
+    public static ConcurrentHashMap<String, University> getUNIVERSITIES() {
         return UNIVERSITIES;
     }
-    
+
 }
