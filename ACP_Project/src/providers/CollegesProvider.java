8f6cebb1f7efcab43ba3ba488260507ff25608bf
Emad Adil Qadr <115411050+Emad-Adil-Qadr@users.noreply.github.com>
2022-11-30 20:14:23 +0300

    Update CollegesProvider.java

--- ACP_Project/src/providers/CollegesProvider.java
+++ ACP_Project/src/providers/CollegesProvider.java
@@ -1,26 +1,32 @@
 package providers;
 
-import university_information.College;
+import Models.College;
+
 import java.io.*;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CollegesProvider {
-    private static HashMap<List<String>, College> COLLEGES = new HashMap<>();
+    private static ConcurrentHashMap<List<String>, College> COLLEGES = new ConcurrentHashMap<>();
     private static boolean isChanged = false;
 
-    public static void fetchColleges() throws IOException, ClassNotFoundException {
-        ObjectInputStream reader = new ObjectInputStream(new FileInputStream("ACP_Project/src/DataFiles/Colleges.txt"));
-        COLLEGES = (HashMap<List<String>, College>) reader.readObject();
-        reader.close();
-        if (isChanged) {
-            isChanged = false;
-            System.out.println("Changes canceled");
+    public static void fetchColleges() {
+        try {
+            ObjectInputStream reader = new ObjectInputStream(new FileInputStream("ACP_Project/src/DataFiles/Colleges.txt"));
+            COLLEGES = (ConcurrentHashMap<List<String>, College>) reader.readObject();
+            reader.close();
+            if (isChanged) {
+                isChanged = false;
+                System.out.println("Changes canceled");
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Cannot fetch colleges: " + e.getMessage());
         }
+
     }
 
-    public static HashMap<List<String>, College> getCOLLEGES() {
+    public static ConcurrentHashMap<List<String>, College> getCOLLEGES() {
         return COLLEGES;
     }
 
@@ -37,17 +43,23 @@ public class CollegesProvider {
             isChanged = true;
         }
     }
-    
-    public static void submitChanges() throws IOException, ClassNotFoundException {
-        if (isChanged) {
-            ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("ACP_Project/src/DataFiles/Colleges.txt"));
-            outputStream.writeObject(COLLEGES);
-            outputStream.close();
-            COLLEGES.clear();
-            isChanged = false;
-            fetchColleges();
-        } else {
-            System.out.println("Please add or remove at least a College");
+
+    public static void submitChanges() {
+        try {
+            if (isChanged) {
+                ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("ACP_Project/src/DataFiles/Colleges.txt"));
+                outputStream.writeObject(COLLEGES);
+                outputStream.close();
+                COLLEGES.clear();
+                isChanged = false;
+                fetchColleges();
+            } else {
+                System.out.println("Please add or remove at least a College");
+            }
+        } catch (IOException e) {
+            System.out.println("Cannot submit changes in colleges: " + e.getMessage());
         }
+
     }
+
 }
