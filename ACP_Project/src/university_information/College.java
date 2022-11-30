59835a21f3c6105b405e2b1b7de750c5420633a9
Twana Abubakr <95369929+twana18@users.noreply.github.com>
2022-11-30 20:03:12 +0300

    Delete ACP_Project/src/university_information directory

--- ACP_Project/src/university_information/College.java
+++
@@ -1,3 +0,0 @@
-package university_information;
-import java.io.Serializable;
-public record College(String id, String name) implements Serializable {}
--- ACP_Project/src/university_information/University.java
+++
@@ -1,5 +0,0 @@
-package university_information;
-
-import java.io.Serializable;
-
-public record University(String id, String name, String location) implements Serializable {}
